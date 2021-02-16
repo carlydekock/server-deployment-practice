2aeb377a502b01dd568bc05ad3ef51aae4fe2b3a
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-02-15 16:47:58 -0800

    added files, tests passing

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response, next) => {
+  response.send('hello');
+});
+
+app.get('/foo', (request, response, next) => {
+  let responseObject = {
+    number: 10,
+    string: 'hello world'
+  }
+  response.json(responseObject);
+});
+
+function start(port) {
+  app.listen(port, () => console.log('App is listening on port: ', port));
+}
+
+module.exports = {
+  app: app,
+  start: start,
+};
